ab2b16fbcd64b6564deb49c1b2a170f32c9d3874
LuisMGS <36911605+LuisMGS@users.noreply.github.com>
2019-06-17 19:12:23 -0500

    Update data_preprocessing_template

--- datasets/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.R
+++ datasets/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.R
@@ -1,6 +1,18 @@
+#------------------------------------------#
 # Plantilla para el Pre Procesado de Datos
+#------------------------------------------#
 # Importar el dataset
 dataset = read.csv('Data.csv')
+
+#Tratamiento de los valores NaN
+dataset$Age = ifelse(is.na(dataset$Age), 
+                     ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
+                     dataset$Age)
+
+dataset$Salary = ifelse(is.na(dataset$Salary), 
+                     ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
+                     dataset$Salary)
+
 #dataset = dataset[, 2:3]
 
 # Dividir los datos en conjunto de entrenamiento y conjunto de test
